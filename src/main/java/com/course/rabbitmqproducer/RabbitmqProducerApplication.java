5d8bd1511936047f240c44fd81846d0672022b60
mecong <mecongsiam@gmail.com>
2021-01-10 17:19:12 +0300

    direct exchange implemented

--- src/main/java/com/course/rabbitmqproducer/RabbitmqProducerApplication.java
+++ src/main/java/com/course/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -1,26 +1,40 @@
 package com.course.rabbitmqproducer;
 
 import com.course.rabbitmqproducer.model.Employee;
+import com.course.rabbitmqproducer.model.Picture;
 import com.course.rabbitmqproducer.producer.EmployeeJsonProducer;
+import com.course.rabbitmqproducer.producer.PictureProducer;
 import com.course.rabbitmqproducer.producer.ResourceJsonProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @SpringBootApplication
 @EnableScheduling
 public class RabbitmqProducerApplication implements CommandLineRunner {
 
+    Logger logger = LoggerFactory.getLogger(RabbitmqProducerApplication.class);
+
     private final EmployeeJsonProducer employeeJsonProducer;
 
     private final ResourceJsonProducer resourceJsonProducer;
 
-    public RabbitmqProducerApplication(EmployeeJsonProducer employeeJsonProducer, ResourceJsonProducer resourceJsonProducer) {
+    private final PictureProducer pictureProducer;
+
+    private List<String> SOURCES= List.of("mobile", "web");
+    private List<String> TYPES= List.of("jpg", "svg", "png");
+
+    public RabbitmqProducerApplication(EmployeeJsonProducer employeeJsonProducer, ResourceJsonProducer resourceJsonProducer, PictureProducer pictureProducer) {
         this.employeeJsonProducer = employeeJsonProducer;
         this.resourceJsonProducer = resourceJsonProducer;
+        this.pictureProducer = pictureProducer;
     }
 
     public static void main(String[] args) {
@@ -34,6 +48,17 @@ public class RabbitmqProducerApplication implements CommandLineRunner {
                     LocalDate.now()));
             resourceJsonProducer.sendMessage(new Employee(Integer.toString(i), "Employee " + i,
                     LocalDate.now()));
+        }
+        for (int i = 0; i < 10; i++){
+            var picture = new Picture();
+
+            picture.setName("Picture "+ i);
+            picture.setSource(SOURCES.get(i%SOURCES.size()));
+            picture.setType(TYPES.get(i%TYPES.size()));
+            picture.setSize(ThreadLocalRandom.current().nextLong(1,10001));
+            logger.info("Picture: {}", picture);
+            pictureProducer.sendPicture(picture);
+
         }
     }
 
